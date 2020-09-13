988684418a1d78af756b912c2042b44d31974b94
Rafal Kostrzynski <kostrzynskirafal@wp.pl>
2020-09-13 16:14:32 +0200

    Initial commit

---
+++ src/main/java/pl/kostrzynski/speechrecognitiontool/SpeechrecognitiontoolApplication.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.speechrecognitiontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechrecognitiontoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeechrecognitiontoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kostrzynski/speechrecognitiontool/SpeechrecognitiontoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.speechrecognitiontool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechrecognitiontoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
